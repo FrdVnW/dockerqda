51ead19e3f9ab2fa28f29decc385eb1dac2f75a2
FrdVnW <f.vanwindekens@gmail.com>
2018-03-21 15:41:37 +0100

    resolution conflit

---
+++ RunDockeRQDA_CRA.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+sudo docker run -it --volume=$XSOCK:$XSOCK:rw \
+     --volume=$XAUTH:$XAUTH:rw \
+     --env="XAUTHORITY=${XAUTH}" \
+     --env="DISPLAY" \
+     --name whirl_wheels \
+     --workdir=/root/ \
+     --volume=/home/fred/Projects/R/dockerqda/:/home/dockerqda/ \
+     dockerqda
+
+
