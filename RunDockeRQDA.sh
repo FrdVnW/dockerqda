a6896461c78f1f103f2ca85d474191e8283302e9
FrdVnW <f.vanwindekens@gmail.com>
2018-04-13 10:38:46 +0200

    remove sudo and link to bash for debug in readme

--- RunDockeRQDA.sh
+++ RunDockeRQDA.sh
@@ -2,11 +2,11 @@
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-sudo docker run --rm -it --volume=$XSOCK:$XSOCK:rw \
-     --volume=$XAUTH:$XAUTH:rw \
-     --env="XAUTHORITY=${XAUTH}" \
-     --env="DISPLAY" \
-     --name whirl_wheels \
-     --workdir=/root/ \
-     --volume=/home/fred/Projects/R/dockerqda/:/home/dockerqda/ \
-     frdvnw/dockerqda:latest
+docker run --rm -it --volume=$XSOCK:$XSOCK:rw \
+       --volume=$XAUTH:$XAUTH:rw \
+       --env="XAUTHORITY=${XAUTH}" \
+       --env="DISPLAY" \
+       --name whirl_wheels \
+       --workdir=/root/ \
+       --volume=/home/fred/Projects/R/dockerqda/:/home/dockerqda/ \
+       frdvnw/dockerqda:latest
